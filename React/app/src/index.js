67fd36da4c3dc4edc8b7cc207424d9cd525d4884
khalilmami007 <136359722+khalilmami007@users.noreply.github.com>
2023-12-12 22:10:59 +0100

    test

---
+++ React/app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const PersonCard = (props) => {
+    return(
+        <div>
+            <h1 style={{color:"red"}}>Instructor Bob</h1>
+            <p>Age: 32</p>
+            <p>Hair Color: purple</p>
+        </div>
+    );
+}
+export default PersonCard;
+
